045fc6a65f068a1c2ef70976b17e8700d8e4a4d9
bobderrico80 <bobderrico80@gmail.com>
2014-08-30 23:39:34 -0400

    Initial Commit

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="<?php get_stylesheet_uri();?>">
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <meta name="viewport" content="width=device-width"/>
+        <title><?php bloginfo('name'); wp_title(" - ",TRUE,'left')?></title>
+    </head>
+    <body class="<?php body_class(); ?>">
+        <div class="pageWrapper">
+            <div class="header">
+
+            </div>
