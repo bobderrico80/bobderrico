ef9d6f5e216bd53826596f8438c11ea91db69eac
bobderrico80 <bobderrico80@gmail.com>
2014-09-04 22:17:54 -0400

    Heading changes
    Title and tagline are now shown on both the page header and the title tag.

--- header.php
+++ header.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="<?php get_stylesheet_uri();?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri();?>">
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <meta name="viewport" content="width=device-width"/>
-        <title><?php bloginfo('name'); wp_title(" - ",TRUE,'left')?></title>
+        <title>
+            <?php 
+                bloginfo('name'); 
+                echo ' &ndash; ';
+                bloginfo('description');
+                wp_title(' - ',TRUE,'left');
+            ?>
+        </title>
     </head>
     <body class="<?php body_class(); ?>">
         <div class="pageWrapper">
             <div class="header">
-                <h1><?php bloginfo('name') ?></h1>
+                <h1 class="headerTitle"><?php bloginfo('name'); ?></h1>
+                <h2 class="headerSubtitle"><?php bloginfo('description'); ?></h2>
             </div>
