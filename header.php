2443e876722f0ba60ba42846381ffb5cae9641f6
Bob D'Errico <bob@bobderrico.com>
2014-10-15 12:16:06 -0400

    Restyled CSS to match old site design
    Also styled some general elements (headings, blockquotes, etc.), and completed styling of summary post view.

--- header.php
+++ header.php
@@ -1,21 +1,32 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri();?>">
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <meta name="viewport" content="width=device-width"/>
-        <title>
-            <?php 
-                bloginfo('name'); 
-                echo ' &ndash; ';
-                bloginfo('description');
-                wp_title(' - ',TRUE,'left');
-            ?>
-        </title>
-    </head>
-    <body class="<?php body_class(); ?>">
-        <div class="pageWrapper">
-            <div class="header">
-                <h1 class="headerTitle"><?php bloginfo('name'); ?></h1>
-                <h2 class="headerSubtitle"><?php bloginfo('description'); ?></h2>
-            </div>
+  <head>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri();?>">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <meta name="viewport" content="width=device-width"/>
+  <title>
+      <?php 
+        bloginfo('name'); 
+        echo ' &ndash; ';
+        bloginfo('description');
+        wp_title(' - ',TRUE,'left');
+      ?>
+    </title>
+  </head>
+  <body class="<?php body_class(); ?>">
+    <div class="pageWrapper">
+      <div class="header">
+        <div class="headerTitleArea">
+          <h1 class="headerTitle"><?php bloginfo('name'); ?></h1>
+          <h2 class="headerSubtitle"><?php bloginfo('description'); ?></h2>
+        </div>
+        <div class="headerNavbarArea">
+          <div class="navbar">
+            <?php wp_page_menu(array(
+              'show_home' => 'Blog'
+           ));?>
+          </div> 
+        </div>
+      </div>
+        
+     
